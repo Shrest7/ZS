52630333748958dfd89e832a51e6ffc3bafa20ec
Shrest7 <dravish@wp.pl>
2017-09-08 18:04:57 +0200

    first commit

---
+++ Długi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Długi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dlug = 5000;
+            int miesiecznaOszczednasc = 300;
+            int zaoszczedzone = 0;
+            int miesiac = 0;
+
+            while (zaoszczedzone<dlug)
+            {
+                zaoszczedzone += miesiecznaOszczednasc;
+                miesiac++;
+                Console.WriteLine("{0} miesiąc spłacono {1} długu.", miesiac, zaoszczedzone);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Dług zostanie spłacony po {0} miesiacach", miesiac);
+            Console.WriteLine("Zostanie mu {0} zł nadwyżki.", (zaoszczedzone-dlug));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
